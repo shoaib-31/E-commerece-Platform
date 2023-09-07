7446a08d03d7281b55f9155660a3da933f21cc77
Shoaib Akhtar <shoaibakmasood@gmail.com>
2023-09-07 23:30:33 +0530

    CORS

--- client/clientconfig.jsx
+++ client/clientconfig.jsx
@@ -1,5 +1,5 @@
 export const clientconfig = {
-  url: "https://e-commerece-platform.vercel.app",
+  url: "https://ecommerce28.azurewebsites.net",
   stripePublic:
     "pk_test_51NnLdDSDMaLUfPc6a7y1pbvSAnuI9akohHguU5kN7a2LErpp3MPW1RDyj0tdsdrgh4VLNzKVxEP2iEISHkGFlolI00xEIBluMN",
 };
