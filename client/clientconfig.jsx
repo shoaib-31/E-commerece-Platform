ee810931cd59878584ee914ce451784e6a8058e2
Shoaib Akhtar <shoaibakmasood@gmail.com>
2023-09-08 10:25:26 +0530

    CORS

--- client/clientconfig.jsx
+++ client/clientconfig.jsx
@@ -1,5 +1,5 @@
 export const clientconfig = {
-  url: "https://ecommerce28.azurewebsites.net",
+  url: "http://localhost:8000",
   stripePublic:
     "pk_test_51NnLdDSDMaLUfPc6a7y1pbvSAnuI9akohHguU5kN7a2LErpp3MPW1RDyj0tdsdrgh4VLNzKVxEP2iEISHkGFlolI00xEIBluMN",
 };
