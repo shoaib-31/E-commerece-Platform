a90e5bc817e28bfe2a01e23c72e792aa1b7342ac
Shoaib Akhtar <shoaibakmasood@gmail.com>
2023-09-08 10:24:08 +0530

    CORS

--- server/server.js
+++ server/server.js
@@ -11,19 +11,27 @@ const paymentRoute = require("./routers/paymentRouter");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+// Specify the allowed origin (replace with your frontend's actual origin)
+const allowedOrigins = ["https://e-commerece-platform-9bee.vercel.app/"]; // Add your frontend's URL here
+
 const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-  credentials: true,
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      // If the origin is in the allowed list or is not defined (e.g., same-origin request), allow it
+      callback(null, true);
+    } else {
+      // If the origin is not allowed, reject the request
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // Allow credentials (cookies)
 };
 
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(bodyParser.json());
 dotenv.config();
-app.get("/", (req, res) => {
-  res.json("Hello World");
-});
+
 app.use("/user", userRoute);
 app.use("/products", productRoute);
 app.use("/orders", orderRoute);
