649641c9843ede0c82879d07f8d6e7f2b31c7aed
Shoaib Akhtar <shoaibakmasood@gmail.com>
2023-09-09 19:34:10 +0530

    Deploy

--- client/clientconfig.jsx
+++ client/clientconfig.jsx
@@ -1,5 +1,5 @@
 export const clientconfig = {
-  url: "https://e-commerce-backend-9hy2.onrender.com",
+  url: "https://e-commerce-backend-rx2r.onrender.com",
   stripePublic:
     "pk_test_51NnLdDSDMaLUfPc6a7y1pbvSAnuI9akohHguU5kN7a2LErpp3MPW1RDyj0tdsdrgh4VLNzKVxEP2iEISHkGFlolI00xEIBluMN",
 };
